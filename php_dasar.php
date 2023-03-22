19640537967709257eeb1f4169cf3b53c03ebf86
Modesta122 <97017179+Modesta122@users.noreply.github.com>
2023-03-22 23:58:17 +0700

    Add files via upload

---
+++ php_dasar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <title>PHP Dasar</title>
+</head>
+<body>
+     <h1>Belajar PHP Dasar</h1>
+     <?php
+         echo "Hello World";
+       ?>
+       <h1 Belajar PHP Dasar</h1>
+       <?php
+    $nim = "312110142";
+    $nama = 'Modesta';
+    echo "NIM : " . $nim . "<br>"; 
+    echo "Nama : $nama";
+     ?>
+</html>
